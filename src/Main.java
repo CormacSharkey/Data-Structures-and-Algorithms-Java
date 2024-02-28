07fa307b011a1eca8ee4dd17e7eb547b49ddc8df
Cormac Sharkey <101349412+CormacSharkey@users.noreply.github.com>
2024-02-28 22:20:36 +0000

    adding more data to linked list example

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,37 @@ import data.structures.MyLinkedList;
 import data.structures.MyQueue;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    System.out.println("Hello world!");
 
-        MyLinkedList<Integer> listy = new MyLinkedList<>();
+    MyLinkedList<Integer> listy = new MyLinkedList<>();
 
-        listy.addLinkEnd(1);
-        listy.addLinkEnd(2);
-        listy.addLinkEnd(3);
+    listy.addLinkEnd(1);
+    listy.addLinkEnd(2);
+    listy.addLinkEnd(3);
+    listy.addLinkEnd(4);
 
-        listy.addLinkStart(0);
-        listy.addLinkStart(-1);
+    listy.addLinkStart(0);
+    listy.addLinkStart(-1);
 
-        listy.deleteLinkStart();
+    listy.deleteLinkStart();
 
-        listy.deleteLinkEnd();
+    listy.deleteLinkEnd();
 
-        listy.showList();
+    listy.showList();
 
-        listy.searchList(1);
-        listy.searchList(-1);
+    listy.searchList(1);
+    listy.searchList(-1);
 
+    MyQueue<Integer> queuey = new MyQueue<>();
 
-        MyQueue<Integer> queuey = new MyQueue<>();
+    queuey.enqueue(1);
+    queuey.enqueue(2);
+    queuey.enqueue(3);
 
-        queuey.enqueue(1);
-        queuey.enqueue(2);
-        queuey.enqueue(3);
+    queuey.dequeue();
 
-        queuey.dequeue();
-
-        queuey.peek();
-    }
+    queuey.peek();
+  }
 }
